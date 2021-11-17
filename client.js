7f8285edd764d16550a0c4b09a8b5a5de1119f7e
Oliveira <jose.m.a.oliveira@accenture.com>
2021-11-17 12:15:48 -0300

    Scrips simples para testar requisição

---
+++ client.js
@@ -0,0 +1,27 @@
+
+//Header da requisição
+var settings = {
+    "url": "...<aqui voce coloca a URL(endereço da API)",
+    "method": "GET",
+    "timeout": 0,
+    "headers": {
+      "Authorization": " <aqui voce coloca o token> "
+    },
+  };
+  
+//Mostrando a requisição na tela
+function consultarApi() {
+    $.ajax(settings).done(function (response) {
+
+   //    $("#conteudo").text(response);
+    var respGrid =  JSON.stringify(response)
+    $("#conteudo").text(respGrid);
+
+       console.log(response)
+
+    });
+
+}
+
+$("#botao").click(consultarApi);
+
